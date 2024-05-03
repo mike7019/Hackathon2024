740517e58976844f0f4232e6967d11679e6e61ad
mike7019 <amph7019@gmail.com>
2024-05-03 16:04:16 -0500

    updates

--- src/main/java/com/co/hackathon/drivers/DriverThreads.java
+++ src/main/java/com/co/hackathon/drivers/DriverThreads.java
@@ -1,18 +1,29 @@
 package com.co.hackathon.drivers;
 
+import com.epam.healenium.SelfHealingDriver;
 import net.serenitybdd.core.Serenity;
 import org.openqa.selenium.WebDriver;
 
+import static com.co.hackathon.drivers.DriversFactory.createWebDriver;
+import static com.co.hackathon.utils.Utils.CHROME;
+
 public class DriverThreads {
 
+    public static WebDriver driver;
     private static final ThreadLocal<WebDriver> WEB_DRIVER_THREAD_LOCAL = new ThreadLocal<>();
 
-    public static void setTheDriver(WebDriver driver){
+    public static void setTheDriver(WebDriver driver) {
         DriverThreads.WEB_DRIVER_THREAD_LOCAL.set(driver);
     }
 
-    public static void setWebDriverScreenshoots(WebDriver driver){
+    public static void setDriverForScreenshots(WebDriver driver) {
         setTheDriver(driver);
         Serenity.getWebdriverManager().setCurrentDriver(driver);
+    }
+
+    public static WebDriver selfHealingDriver(String browser) {
+        driver = createWebDriver(browser);
+        DriverThreads.setDriverForScreenshots(driver);
+        return SelfHealingDriver.create(driver);
     }
 }
--- src/test/java/com/co/hackathon/stepdefinitions/Hooks.java
+++ src/test/java/com/co/hackathon/stepdefinitions/Hooks.java
@@ -1,22 +1,18 @@
 package com.co.hackathon.stepdefinitions;
 
-import com.co.hackathon.drivers.DriverThreads;
-import com.co.hackathon.drivers.DriversFactory;
-import com.epam.healenium.SelfHealingDriver;
 import io.cucumber.java.Before;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.Cast;
 import net.serenitybdd.screenplay.actors.OnStage;
-import org.openqa.selenium.WebDriver;
 
+import static com.co.hackathon.drivers.DriverThreads.selfHealingDriver;
 import static com.co.hackathon.utils.Utils.CHROME;
 
 public class Hooks {
+
     @Before
     public void setTheStage(){
-        WebDriver driver = DriversFactory.createWebDriver(CHROME);
-        DriverThreads.setWebDriverScreenshoots(driver);
-        WebDriver healerDriver = SelfHealingDriver.create(driver);
-        OnStage.setTheStage(Cast.whereEveryoneCan(BrowseTheWeb.with(healerDriver)));
+        OnStage.setTheStage(Cast.whereEveryoneCan(BrowseTheWeb.with(selfHealingDriver(CHROME))));
     }
+
 }
